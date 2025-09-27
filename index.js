5b10be489637f1cfa762faf07671682b47df74a4
llyman707 <llyman707@gmail.com>
2025-09-27 13:05:53 -0400

    refactoring to model

--- index.js
+++ index.js
@@ -38,11 +38,7 @@ app.get('/register', (req, res) => {
 
 app.post('/register', async (req, res) => {
     const { password, username } = req.body;
-    const hash = await bycrypt.hash(password, 12);
-    const user = new User({
-        username,
-        password: hash
-    })
+    const user = new User({username, password})
     await user.save();
     req.session.user_id = user._id;
     res.redirect('/')
@@ -54,10 +50,11 @@ app.get('/login', (req, res) => {
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
+    const foundUser = await User.findAndValidate(username, password)
     const user = await User.findOne({ username });
     const validPassword = await bycrypt.compare(password, user.password);
-    if (validPassword) {
-        req.session.user_id = user._id;
+    if (foundUser) {
+        req.session.user_id = foundUser._id;
         res.redirect('/secret')
     }
     else {
